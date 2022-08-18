f1a513249e1e978b787e53657a4b7517a5c1c235
sabegomaa261 <sabergomaa261@gmail.com>
2022-08-18 11:35:13 +0200

    query expression

--- Program.cs
+++ Program.cs
@@ -11,13 +11,23 @@ namespace EntityFramwork.net5 // .net5 == .net core
             #region eager loading
 
             var query =
-                context.Departments.AsSplitQuery() // 
+                context.Departments.AsSplitQuery() // to split and filter quers
                 .Include(e => e.projects)
                 .Include(d => d.Empolyees)
                 .ThenInclude(p => p.Attendances).ToList(); // from last included
 
             #endregion
 
+            var query1 =
+                from emps in context.Empolyee 
+                where emps.ID > 2
+                select new { emps.ID, EmpolyeeName = emps.Name , emps.Salary };
+
+
+            foreach(var e in query1)
+            {
+                Console.WriteLine($"Id {e.ID} \t Name {e.EmpolyeeName} \t Salary {e.Salary}");
+            }
 
             // lazy loading prevented in EF Core
             //foreach (var dept in query)
